c5848d4deda7119695e2b17d9b374c81e4ca0602
Lucas-Ying <lucas.yingmm@gmail.com>
2017-04-11 19:58:33 +1200

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Blog.create(
+          title: "My Blog Post #{i}",
+          body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+puts "10 blog posts created."
+
+5.times do |i|
+  Skill.create!(
+           title: "Skill #{i}",
+           percent_utilized: 15
+  )
+end
+
+puts "5 skills created."
+
+9.times do |i|
+  Portfolio.create!(
+               title: "Portfolio title: #{i}",
+               subtitle: "My great service",
+               body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               main_image: "http://placehold.it/600x250",
+               thumb_image: "http://placehold.it/350x150"
+  )
+end
+
+puts "9 portfolio items created."
